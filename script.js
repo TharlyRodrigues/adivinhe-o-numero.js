2b260111ece6832592e194ee9a812b091c68949b
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-09 20:51:59 -0300

    adicionando as mecânicas do game

--- script.js
+++ script.js
@@ -10,11 +10,47 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretnumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+// let Highscore = ;
+document.querySelector('.number').textContent = secretnumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // when there is input
   if (!guess) {
     document.querySelector('.message').textContent = '❌ No Number!';
+
+    // when player wins
+  } else if (guess === secretnumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high or low
+  } else if (guess > secretnumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Hight 📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🎆You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#ff0000';
+    }
+  } else if (guess < secretnumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low 📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🎆You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#ff0000';
+    }
   }
 });
