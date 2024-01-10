13c359aaa860cd47657dba2a1b1bfec5ce047649
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-10 16:35:59 -0300

    update refaturamento

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let secretnumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
-// let Highscore = ;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -24,30 +23,39 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-
-    // when guess is too high or low
-  } else if (guess > secretnumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too Hight 📈';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '🎆You lost the game!';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#ff0000';
-    }
-  } else if (guess < secretnumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low 📉';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '🎆You lost the game!';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#ff0000';
-    }
+  } else if (score > 1) {
+    document.querySelector('.message').textContent =
+      guess > secretnumber ? 'Too High 📈' : 'Too Low 📉';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = '🎆You lost the game!';
+    document.querySelector('.score').textContent = 0;
+    document.querySelector('body').style.backgroundColor = '#ff0000';
   }
 });
+
+//   // when guess is too high or low
+// } else if (guess > secretnumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = 'Too Hight 📈';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = '🎆You lost the game!';
+//     document.querySelector('.score').textContent = 0;
+//     document.querySelector('body').style.backgroundColor = '#ff0000';
+//   }
+// } else if (guess < secretnumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = 'Too low 📉';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = '🎆You lost the game!';
+//     document.querySelector('.score').textContent = 0;
+//     document.querySelector('body').style.backgroundColor = '#ff0000';
+//   }
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
