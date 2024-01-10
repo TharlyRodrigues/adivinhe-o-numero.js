aa305f9f7af0adecb6fd6026b9b1323d981fa0f6
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-09 21:24:23 -0300

    adicionando a função higtscore

--- script.js
+++ script.js
@@ -10,13 +10,12 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretnumber = Math.trunc(Math.random() * 20) + 1;
+let secretnumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = 0;
 
 // let Highscore = ;
-document.querySelector('.number').textContent = secretnumber;
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -28,8 +27,13 @@ document.querySelector('.check').addEventListener('click', function () {
     // when player wins
   } else if (guess === secretnumber) {
     document.querySelector('.message').textContent = '🎉Correct Number';
+    document.querySelector('.number').textContent = secretnumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    // Incrementando o high score
+    highscore++;
+    document.querySelector('.highscore').textContent = highscore;
 
     // when guess is too high or low
   } else if (guess > secretnumber) {
@@ -53,4 +57,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('body').style.backgroundColor = '#ff0000';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretnumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
